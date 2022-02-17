7f3734424fab61d737216cfd54bc8624df11d459
deathang3l <tomasz.lis.pl@gmail.com>
2022-02-17 08:38:52 +0100

    Again

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,16 @@ class ArticlesController < ApplicationController
   def show
     @mArticle = Article.find(params[:id])
   end
+
   def index
     @mArticles = Article.all
+  end
+
+  def new
+
+  end
+
+  def create
+
   end
 end
