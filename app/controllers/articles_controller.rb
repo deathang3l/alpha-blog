06aa34377d7e95060f3f1a3c96b1294f38248fc3
deathang3l <tomasz.lis.pl@gmail.com>
2022-02-22 12:14:52 +0100

    Just

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
       flash[:notice] = "(V) Article was created successfully."
       redirect_to @mArticle
     else
-      flash[:notice] = "(X) Article was not created:<br>#{@mArticle.errors.to_a.join('<br>')}"
+      flash[:notice] = "(X) Article was not created:<br>#{@mArticle.errors.to_a.join('\n-')}"
       render 'new'
     end
   end
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
       flash[:notice] = "(V) Article was updated successfully."
       redirect_to @mArticle
     else
-      flash[:notice] = "(X) Article was not updated:<br>#{@mArticle.errors.to_a.join('<br>')}"
+      flash[:notice] = "(X) Article was not updated:<br>#{@mArticle.errors.to_a.join('\n-')}"
       render 'edit'
     end
   end
