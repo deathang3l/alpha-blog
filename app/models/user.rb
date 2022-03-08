9f918bf5df3e7e065796ff932d1c519379a25620
deathang3l <tomasz.lis.pl@gmail.com>
2022-03-08 09:11:12 +0100

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-
+  EMAIL_VALID_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username, presence: true, uniqueness: {case_sensitive: false},
+                       length: {minimum: 3, maximum: 25}
+  validates :email, presence: true, uniqueness: {case_sensitive: false},
+                    length: {maximum: 105}, format: {with: EMAIL_VALID_REGEX}
 end
-
