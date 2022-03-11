b337c70379e2e74210110b8982db0d696a0b3824
deathang3l <tomasz.lis.pl@gmail.com>
2022-03-11 10:57:59 +0100

    Pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,11 +28,11 @@ class UsersController < ApplicationController
   end  
 
   def show
-    @mArticles = @mUser.articles
+    @mArticles = @mUser.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
-    @mUsers = User.all
+    @mUsers = User.paginate(page: params[:page], per_page: 5)
   end
 
   private
