15ba7c938ded42da2dfddee252336d6b9cbd7a19
deathang3l <tomasz.lis.pl@gmail.com>
2022-03-15 11:47:31 +0100

    Session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    input = params[:session][:email]
+    if input.includes? '@' then
+      user = User.find(email: input.downcase)
+    else
+      user = User.find(username: input)
+    end
+    if user && use.authenticate(params[:session][:password]) then
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "There was something wrong with your login details"
+      render 'new'
+    end
   end
 
   def destroy
